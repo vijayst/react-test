bb57dc4774f3b9c42a851c35a8f2d8825e7d92bc
vijayst <vijayst@gmail.com>
2016-09-05 22:16:10 +0530

    A simple react component and webpack config.

---
+++ src/client/app.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = (props) => (
+  <div>Hello world</div>
+);
+
+ReactDOM.render(<App />, document.getElementById('app'));
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+var path = require('path');
+
+module.exports = {
+  context: path.resolve('src/client'),
+  entry: ['./app.jsx'],
+  output: {
+    path: path.resolve('build/client/'),
+    publicPath: '/scripts',
+    filename: 'bundle.js',
+  },
+  devServer: {
+    contentBase: 'src/public'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.(jsx|js)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  }
+};
