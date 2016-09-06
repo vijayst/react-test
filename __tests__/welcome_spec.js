9589db5fb6d1f152210652a147305c0fa7849d38
vijayst <vijayst@gmail.com>
2016-09-06 12:21:38 +0530

    react tests with mocha.

--- __tests__/welcome_spec.js
+++ __tests__/welcome_spec.js
@@ -3,8 +3,10 @@ import ReactDOM from 'react-dom';
 import TestUtils from 'react-addons-test-utils';
 import Welcome from '../src/client/components/welcome.jsx';
 
-it('Welcome renders hello world', () => {
-  const welcome = TestUtils.renderIntoDocument(<Welcome />);
-  const welcomeNode = ReactDOM.findDOMNode(welcome);
-  expect(welcomeNode.textContent).toEqual('Hello world');
+describe('Welcome', () => {
+  it('Welcome renders hello world', () => {
+    const welcome = TestUtils.renderIntoDocument(<Welcome />);
+    const welcomeNode = ReactDOM.findDOMNode(welcome);
+    expect(welcomeNode.textContent).toEqual('Hello world');
+  });
 });
