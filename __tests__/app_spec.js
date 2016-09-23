6788dbc92f461a3a5ca9336bd5d6a7c631a8aa35
vijayst <vijayst@gmail.com>
2016-09-23 23:59:38 +0530

    updated app spec.

--- __tests__/app_spec.js
+++ __tests__/app_spec.js
@@ -1,33 +1,28 @@
 import React from 'react';
-import TestUtils from 'react-addons-test-utils';
-import Add from '../src/client/components/add.jsx';
+import { shallow } from 'enzyme';
 import App from '../src/client/components/app.jsx';
-import List from '../src/client/components/list.jsx';
 
 describe('App', () => {
-  let renderer;
   let app;
 
   beforeEach(() => {
-    renderer = TestUtils.createRenderer();
-    renderer.render(<App />);
-    app = renderer.getRenderOutput();
+    app = shallow(<App />);
   });
 
-  it('Add renders nested components', () => {
-    const { children } = app.props;
-    expect(children.length).toEqual(2);
-    expect(TestUtils.isElementOfType(children[0], Add)).toBe(true);
-    expect(TestUtils.isElementOfType(children[1], List)).toBe(true);
+  it('App renders nested components', () => {
+    expect(app.find('Add').length).toEqual(1);
+    expect(app.find('List').length).toEqual(1);
   });
 
   it('onAdd updates List', () => {
-    const { children } = app.props;
-    children[0].props.onAdd('Name 1');
-    const newApp = renderer.getRenderOutput();
-    const list = newApp.props.children[1].props.data;
-    expect(list.length).toEqual(1);
-    expect(list[0]).toEqual('Name 1');
+    const add = app.find('Add').first();
+    add.props().onAdd('Name 1');
+    app.update();
+
+    const list = app.find('List').first();
+    const listData = list.props().data;
+    expect(listData.length).toEqual(1);
+    expect(listData[0]).toEqual('Name 1');
   });
 
 });
